cfbefad2e392beb11bed61030ba6859083580305
tcatsl <thomas.castleman@gmail.com>
2017-03-20 16:59:51 -0600

    testing

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var users = require('./routes/users');
 var stormpath = require('express-stormpath');
 var app = express();
 var corsOptions = {
-  origin: 'https://api.stormpath.com',
+  origin: '*',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
