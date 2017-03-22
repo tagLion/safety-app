0c515461e733d1daaef51367a02a2498c6a305eb
tcatsl <thomas.castleman@gmail.com>
2017-03-22 11:47:56 -0600

    sms functionality

--- routes/incidents.js
+++ routes/incidents.js
@@ -3,7 +3,8 @@ var router = express.Router()
 var path = require('path')
 var knex = require('../db/knex.js')
 var stormpath = require('express-stormpath')
-
+require('dotenv').config()
+var client = require('twilio')(process.env.TWI1, process.env.TWI2)
 router.get('/:id', (req, res) => {
   var incidentID = req.params.id
 
@@ -13,12 +14,34 @@ router.get('/:id', (req, res) => {
     })
 })
 
-router.post('/', (req, res) => {
+router.post('/', stormpath.getUser, (req, res) => {
   var userID = req.body.user_id
-  knex('incident').insert({user_id:userID}).returning(['id'])
+  knex.select('id').from('user').where('email', res.locals.user.email)
+  .then(result =>{
+    if (result.length > 0){
+    userID = result[0].id
+    console.log(result[0])
+  } else {userID = 1}
+    knex('incident').insert({user_id:userID}).returning(['id'])
     .then(result => {
       res.send(result)
+      knex.select('phone').from('eContact').where('user_id', userID)
+      .then(phoneNums=>{
+        phoneNums.forEach(function (el, ind, arr){
+          console.log(el)
+          client.sendMessage({
+            to: '+1'+el.phone+'',
+            from: '+17209034041',
+            body: 'Hello from GetMeSafe'
+          }, function(err,data){
+            if(err)
+            console.log(err);
+            console.log(res.status(200).send());
+          })
+        })
+      })
     })
+  })
 })
 
 router.patch('/:id', (req, res) => {
