78f9a9a697a6ce27fbb22cd39862c92c738bb7cc
tcatsl <thomas.castleman@gmail.com>
2017-03-22 20:45:06 -0600

    implement email

--- routes/incidents.js
+++ routes/incidents.js
@@ -6,6 +6,9 @@ var stormpath = require('express-stormpath')
 var shortid = require('shortid')
 require('dotenv').config()
 var client = require('twilio')(process.env.TWI1, process.env.TWI2)
+var api_key = process.env.MGAPIKEY;
+var domain = process.env.DOMAIN;
+var nodemailer = require('nodemailer');
 router.get('/:id/:key', (req, res) => {
 
   var incidentID = req.params.id
@@ -27,6 +30,7 @@ router.get('/endincident/:id', (req, res) => {
     .then(endTime => {
       res.send(endTime[0]["end_timestamp"])
     })
+    .catch(res.sendStatus(404))
 })
 router.post('/', stormpath.getUser, (req, res) => {
   var filler = 'test'
@@ -46,19 +50,63 @@ router.post('/', stormpath.getUser, (req, res) => {
       var inc = results[0].id
       var key = results[0].key
       console.log(key)
-      knex.select('phone').from('eContact').where('user_id', userID)
-      .then(phoneNums=>{
+      knex.select('email', 'phone').from('eContact').where('user_id', userID)
+      .then(emails=>{
         if (userID !== 1){
-        phoneNums.forEach(function (el, ind, arr){
+          emails.forEach(function(el, ind, arr){
+            // var data = {
+            //   from: 'GetMeSafe <postmaster@'+process.env.DOMAIN+'>',
+            //   to: el.email,
+            //   subject: 'GetMeSafe Alert',
+            //   text: 'Hello from https://getmesafe.herokuapp.com. \n'+fn+ ' '+ln+' has currently indicated that they are in an unsafe situation and has you listed as an emergency contact. To track their location, visit https://getmesafe.herokuapp.com/trackpath.html?id='+inc+'&key='+key
+            // };
+            // mailgun.messages().send(data, function (error, body) {
+            //   console.log(body);
+            //   if(!error)
+            //     console.log('email sent')
+            //   else {
+            //     console.log('email error')
+            //   }
+            // });
+            var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: process.env.GACC, // Your email id
+            pass: process.env.GPASS // Your password
+        }
+      })
+        var mailOptions = {
+    from: process.env.GACC, // sender address
+    to: el.email, // list of receivers
+    subject: 'GetMeSafe Alerts', // Subject line
+    text: 'Hello from https://getmesafe.herokuapp.com. \n'+fn+ ' '+ln+' has currently indicated that they are in an unsafe situation and has you listed as an emergency contact. To track their location, visit https://getmesafe.herokuapp.com/trackpath.html?id='+inc+'&key='+key
+
+};
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.log('mail error')
+        console.log(error);
+        res.json({yo: 'error'});
+    }else{
+        console.log('Message sent: ' + info.response);
+        res.json({yo: info.response});
+    };
+});
+          })
+
+
+        emails.forEach(function (el, ind, arr){
           console.log(el)
           client.sendMessage({
             to: '+1'+el.phone+'',
             from: '+17209034041',
-            body: 'Hello from GetMeSafe. \n'+fn+' '+ln+' is feeling unsafe. Track their location at: https://getmesafe.herokuapp.com/trackpath.html?id='+inc+'&key='+key
+            body: 'Hello from https://getmesafe.herokuapp.com. \n'+fn+ ' '+ln+' has currently indicated that they are in an unsafe situation and has you listed as an emergency contact. To track their location, visit https://getmesafe.herokuapp.com/trackpath.html?id='+inc+'&key='+key
           }, function(err,data){
-            if(err)
-            console.log(err);
-            console.log(res.status(200).send());
+            if(err){
+            console.log('sms error');
+          } else {
+            console.log('sms success')
+          }
           })
         })
       }
